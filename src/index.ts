14cf2a8937a707f5b0437e7749cfa2945642aa1b
deepak-deez <deezpatra2015@gmail.com>
2023-07-25 13:22:44 +0530

    created snowpack bundle and configured

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,61 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+console.log("hello");
 
-import confetti from 'canvas-confetti';
+console.log("hey");
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+
+const todoList = document.querySelector<HTMLUListElement>('.todo-list')
+const todoForm = document.querySelector<HTMLFormElement>('.new-todo-form')
+// document.getElementById('#new-todo-form') as HTMLFormElement | null
+const inputTodo = document.querySelector<HTMLInputElement>('.input-todo')
+
+// const addButton = document.querySelector('.add-btn');
+// const todosArray = []
+// const todosObj ={
+//     id: null,
+//     text: null,
+//     completed: false
+// }
+
+todoForm?.addEventListener('submit' , (e)=>{
+e.preventDefault();
+console.log("hiiiiii");
+if(inputTodo?.value === "" || inputTodo?.value=== null)
+  {
+    const todo = {
+      id: Date.now(),
+      title: inputTodo.value,
+      completed: false, 
+      createdAt: new Date()
+    }
+  }
+
+
+})
+
+
+// addButton.onclick = ()=>{
+    // todosObj
+    // const todoElement = document.createElement('li');
+    // const todoElementDelete = document.createElement('button');
+    // const todoElementText = document.createElement('p');
+    // todoElementText.innerHTML = inputTodo.value
+    // todoElementDelete.innerHTML="Delete";
+    // todoElementDelete.classList.add('delete-btn')
+    // todoElementDelete.setAttribute('id', Date.now())
+    // todoElement.appendChild(todoElementText)
+    // todoElement.appendChild(todoElementDelete)
+    // todoElement.classList.add('flex')
+    // todoElement.setAttribute('id', Date.now());
+    // todoList.appendChild(todoElement);
+    // inputTodo.value="";
+
+    // const deleteButton = document.querySelectorAll('.delete-btn');
+
+    // deleteButton.forEach((ele)=>ele.onclick = () =>{
+    //     todoElement.remove(ele.parentElementNode)
+    //     console.log(ele.id);
+    // })
+
+// }
+
+// export {}
